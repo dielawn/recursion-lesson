7b0e7550828bb49559766ec410d267207ae3b27a
dielawn <dielawnmerkill@gmail.com>
2023-07-20 19:47:36 -0600

    factorial functions and other recursive lessons

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ function countDownRecursive(n) {
         console.log('we made it out recursively')
         return
     }
-    console.log(n)
     countDownRecursive(n - 1)
 }
 countDownRecursive(10) 
@@ -21,12 +20,85 @@ countDownRecursive(10)
 
 function sumRange(n) {
     let total = 0
-    for (let i = 0; i > 0; i--) {
+    for (let i = n; i > 0; i--) {
         total += i
-        console.log(total)
     }
-    console.log(total)
     return total
 }
 
-sumRange(5)
+console.log(sumRange(3))
+
+function sumRangeRecursive(n, total = 0) {
+    if (n <= 0) {
+        console.log('the end')
+        return total
+    }    
+    return sumRangeRecursive(n - 1, total + n)
+}
+
+console.log(sumRangeRecursive(3))
+
+
+function printChildren(t) {
+
+}
+
+const tree = {
+    name: 'John', 
+    children: [
+        {
+            name: 'Jim',
+            children: []
+        },
+        {
+            name: 'Zoe',
+            children: [
+                {name: 'Kyle', children: [] },
+                {name: 'Sophia', children: [] }
+            ]
+        }
+    ]
+}
+
+function printChildrenRecursive(t) {
+    if (t.children.length === 0) {
+        return
+    }
+    t.children.forEach(child => {
+        console.log(child.name)
+        printChildrenRecursive(child)
+    })
+}
+printChildrenRecursive(tree)
+
+
+// four factotrial  4! === 4 * 3 * 2 * 1 = 24
+
+function factotrial(n) {
+    if (n <= 0) 
+        return 1
+    else 
+        return n * factotrial(n-1)    
+}
+console.log(factotrial(4))
+
+
+
+function isEven(number) {
+    return number % 2 === 0;
+  }
+  function isOdd(number) {
+    return number % 2 !== 0;
+  }
+
+function toOneRecursively(n) {
+    if (n === 1) {
+       return 1
+    }
+    if (isEven(n)) 
+        return  toOneRecursively(n / 2)
+       else if (isOdd(n))
+         return toOneRecursively(3 * n + 1)
+}
+
+console.log(toOneRecursively(10))
